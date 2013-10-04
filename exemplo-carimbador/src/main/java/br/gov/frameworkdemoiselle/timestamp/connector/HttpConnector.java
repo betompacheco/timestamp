fd6cb9e9dc83de3ffb7f201bedd6a45416febb0e
Humberto Pacheco <07721825741@serpro-1538650.rjo.serpro>
2013-10-04 16:27:15 -0300

    Versao inicial do artefato

--- exemplo-carimbador/src/main/java/br/gov/frameworkdemoiselle/timestamp/connector/HttpConnector.java
+++ exemplo-carimbador/src/main/java/br/gov/frameworkdemoiselle/timestamp/connector/HttpConnector.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.gov.frameworkdemoiselle.timestamp.connector;
 
 import java.io.InputStream;
@@ -14,21 +10,21 @@ public class HttpConnector implements Connector {
 
     @Override
     public InputStream connect(byte[] content) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void setHostname(String hostname) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void setPort(int port) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void close() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
