b48e8d0d0d2977f856f7c11a60f19c5a12904608
Humberto Pacheco <07721825741@serpro-1538650.rjo.serpro>
2013-10-04 16:41:48 -0300

    Versao inicial do artefato

---
+++ exemplo-carimbador/src/main/java/br/gov/frameworkdemoiselle/timestamp/connector/ConnectorFactory.java
@@ -0,0 +1,27 @@
+package br.gov.frameworkdemoiselle.timestamp.connector;
+
+import br.gov.frameworkdemoiselle.timestamp.enumeration.ConnectionType;
+
+/**
+ *
+ * @author 07721825741
+ */
+public class ConnectorFactory {
+
+    public static Connector buildConnector(ConnectionType connectionType) {
+
+        switch (connectionType) {
+
+            case HTTP: {
+                return new HttpConnector();
+            }
+
+            case SOCKET: {
+                return new SocketConnector();
+            }
+        }
+
+        return null;
+
+    }
+}
